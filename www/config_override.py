d917b8fadfee52ffb46bf0df9889975b61851fb2
Magina <815831534@qq.com>
2019-03-20 14:57:27 +0800

    add config_override

---
+++ www/config_override.py
@@ -0,0 +1,17 @@
+##!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'user': 'root',
+        'password': '123456',
+        'db': 'mysql'
+    }
+}
